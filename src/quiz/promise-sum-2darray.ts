c2bea62e3285a9f69c9a9972bbe198569d734bd2
Principe2014 <112664140+Principe2014@users.noreply.github.com>
2024-10-20 19:52:31 -0400

    Update promise-sum-2darray.ts

--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -1,5 +1,25 @@
+function suma2DArray(array: number[][]): Promise<number> {
+    const promises = array.map((row) => {
+        return Promise.resolve(row.reduce((sum, num) => sum + num, 0));
+    });
+
+    return Promise.all(promises).then((rowSums) => {
+        return rowSums.reduce((sum, rowSum) => sum + rowSum, 0);
+    });
+}
+
+
 const array2D_1 = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+
+suma2DArray(array2D_1)
+  .then((sum) => {
+    console.log('Sum of the 2D array:', sum);
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
